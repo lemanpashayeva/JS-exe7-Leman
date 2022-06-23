51b0a530346c3724f495823f141e564f0d37b850
lemanpashayeva <lmnpasayeva@gmail.com>
2022-06-23 12:34:18 +0400

    second repository

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//tap-4
+let lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, distinctio omnis volubtatem deleniti placeat eligendi lorem, earum deserunt delectus sequi officiis iste possimus dolore a temporibus aperiam lorem ipsa? Corporis.";
+console.log(lorem);
+console.log(lorem.replace(/LOREM/ig, "Leman"));
