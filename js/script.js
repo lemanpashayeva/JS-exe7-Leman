db29fb3facab73a1578b8ff63c3a6215c0540d45
lemanpashayeva <lmnpasayeva@gmail.com>
2022-06-23 12:50:44 +0400

    third repository

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 let lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, distinctio omnis volubtatem deleniti placeat eligendi lorem, earum deserunt delectus sequi officiis iste possimus dolore a temporibus aperiam lorem ipsa? Corporis.";
 console.log(lorem);
 console.log(lorem.replace(/LOREM/ig, "Leman"));
+
+//tap-3 
+function firstChar(){
+    let ad = " Leman Pashayeva ";
+    let name = ad.trim();
+    let char = name.charAt(0);
+    console.log(char);
+}
